73114577ff6681e26fb9af6f4572d14a0e4c337e
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-20 12:30:38 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
 const Header = () => {
-  return <div className="h-40 flex items-center justify-center text-6xl font-bold"><span className="text-red-500">E</span>n<span className="text-green-500">g</span>l<span className="text-yellow-300">i</span>s <span className="text-blue-500">h</span> &nbsp; C<span className="text-fuchsia-500">a</span>r <span className="text-lime-400">d</span>s</div>;
+  return (
+    <div className="h-40 flex items-center justify-center text-6xl font-bold">
+      <span className="text-red-500">E</span>n
+      <span className="text-green-500">g</span>l
+      <span className="text-yellow-300">i</span>s{" "}
+      <span className="text-blue-500 transform rotate-[-20deg]">h</span> &nbsp;
+      C<span className="text-fuchsia-500">a</span>r{" "}
+      <span className="text-lime-400">d</span>
+      <span className="transform rotate-[20deg]">s</span>
+    </div>
+  );
 };
 
 export default Header;
+
+
