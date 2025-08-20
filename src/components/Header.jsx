6c43680fefaaf54fff74e6cdfacc3307dda6dd73
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-20 17:48:19 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
       <span className="text-green-500">g</span>l
       <span className="text-yellow-300">i</span>s{" "}
       <span className="text-blue-500 transform rotate-[-20deg]">h</span> &nbsp;
-      C<span className="text-fuchsia-500">a</span>r{" "}
+      C<span className="text-fuchsia-500 rotate-[-20deg]">a</span>r{" "}
       <span className="text-lime-400">d</span>
       <span className="transform rotate-[20deg]">s</span>
     </div>
