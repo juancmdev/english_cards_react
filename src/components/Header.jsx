d2c9970967d21a0c1a110a8e91765dffb81dc3c7
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-20 12:20:32 -0500

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="h-40 flex items-center justify-center text-6xl font-bold"><span className="text-red-500">E</span>n<span className="text-green-500">g</span>l<span className="text-yellow-300">i</span>s <span className="text-blue-500">h</span> &nbsp; C<span className="text-fuchsia-500">a</span>r <span className="text-lime-400">d</span>s</div>;
 };
 
 export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <>
       <nav className="h-12 bg-blue-500 mb-6 text-white font-bold shadow-amber-100 shadow-md flex justify-around items-center">
-        <Link to="/" className="shadow-amber-100 shadow-md">Vocabulary</Link>
+        <Link to="/">Vocabulary</Link>
         <Link to="/verbtobe">Verb To Be</Link>
         <Link to="/verbtohave">Verb To Have</Link>
         <Link to="/wordsoftheweek">Words Of The Week</Link>
