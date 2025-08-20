ce01490b0509ef105971666c9e73f3540d6b25ba
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-20 12:08:39 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      <nav>
-        <Link to="/">Vocabulary</Link>
+      <nav className="h-12 bg-blue-500 mb-6 text-white font-bold shadow-amber-100 shadow-md flex justify-around items-center">
+        <Link to="/" className="shadow-amber-100 shadow-md">Vocabulary</Link>
         <Link to="/verbtobe">Verb To Be</Link>
         <Link to="/verbtohave">Verb To Have</Link>
         <Link to="/wordsoftheweek">Words Of The Week</Link>
