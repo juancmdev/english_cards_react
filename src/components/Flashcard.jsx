0ad66d8fe4539821083a775a27921765613da588
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-23 14:37:27 -0500

    Update Flashcard.jsx

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,27 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Flashcard = ({ cardData }) => {
-const [isFlipped, setIsFlipped] = useState(false);
-const { english, spanish } = cardData;
+  const [isFlipped, setIsFlipped] = useState(false);
+  const { english, spanish } = cardData;
 
-const timeOut = () =>{
-    setIsFlipped(false) 
-};
+  // Lógica de useEffect para el temporizador
+  useEffect(() => {
+    let timeoutId;
+    if (isFlipped) {
+      timeoutId = setTimeout(() => {
+        setIsFlipped(false);
+      }, 2000); // Voltea de vuelta después de 5 segundos
+    }
 
-return (
-    <>
-        <div className={`text-4xl font-bold h-80 w-80 cursor-pointer bg-white rounded-xl shadow-lg relative [perspective:1000px] [transform-style:preserve-3d] transition-transform duration-500 ease-in-out mx-auto ${isFlipped ? "rotate-y-180" : ""}`}
-            onClick={() => setIsFlipped(!isFlipped)}>
-            <div className={`front absolute top-0 left-0 [backface-visibility:hidden] flex justify-center items-center h-full w-full transform -rotate-y-360`}>
-                {spanish}
+    // Función de limpieza para cancelar el temporizador si el componente se desmonta
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isFlipped]);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+         <>
+            <div className={`text-4xl font-bold h-80 w-80 cursor-pointer bg-white rounded-xl shadow-lg relative [perspective:1000px] [transform-style:preserve-3d] transition-transform duration-500 ease-in-out mx-auto ${isFlipped ? "rotate-y-180" : ""}`}
+                onClick={handleFlip}>
+                <div className={`front absolute top-0 left-0 [backface-visibility:hidden] flex justify-center items-center h-full w-full transform -rotate-y-360`}>
+                    {spanish}
+                </div>
+                <div className={`back [backface-visibility:hidden] absolute top-0 left-0  flex justify-center items-center h-full w-full transform -rotate-y-180`}>
+                    {english}
+                </div>
             </div>
-            <div className={`back [backface-visibility:hidden] absolute top-0 left-0  flex justify-center items-center h-full w-full transform -rotate-y-180  ${isFlipped ? setTimeout(timeOut, 3000) : ""}`}>
-                {english}
-            </div>
-        </div>
-    </>
-    );
-};
+        </>
+        );
+    };
 
 export default Flashcard;
 
