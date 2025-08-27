0e588601dc9ec5da8f6c7c70e9a31ac0a236712e
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-27 17:26:37 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 
+
 const links = [
+  { 
+    text: "Home", 
+    path: "/", 
+    id: 0, 
+    icon: "M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" 
+  },
   { text: "Vocabulary", path: "/vocabulary", id: 1 },
   { text: "Verb To Be", path: "/verbtobe", id: 2 },
   { text: "Verb To Have", path: "/verbtohave", id: 3 },
   { text: "Words Of The Week", path: "/wordsoftheweek", id: 4},
 ];
+
+const icons = `<svg class="nav-icon" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+<path d="${links[0].text}"></path>
+</svg>`
 
 
 const Nav = () => {
