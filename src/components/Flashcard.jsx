d664b6456e3713ad2c7e17b1893373d9b01f62f4
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-27 15:25:20 -0500

    Update Flashcard.jsx

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -23,24 +23,27 @@ const Flashcard = ({ cardData }) => {
     setIsFlipped(!isFlipped);
   };
 
+ 
+
   return (
          <>
             <div className={`text-4xl mt-6 mb-6 font-bold h-80 w-80 cursor-pointer bg-white rounded-xl shadow-lg relative [perspective:1000px] [transform-style:preserve-3d] transition-transform duration-500 ease-in-out mx-auto ${isFlipped ? "rotate-y-180" : ""}`}
                 onClick={handleFlip}>
-                {/* <div className={`front p-6 absolute top-0 left-0 [backface-visibility:hidden] flex justify-center items-center h-full w-full transform -rotate-y-360`}>
-                  <div className="flex flex-col items-center">
-                    {spanish === "" ? <img src={urlImage} alt={spanish} className="mt-4 max-h-full max-w-full"></img> : spanish}
-                </div>
-                  </div> */}
+  
 
-<div className={`front p-6 absolute top-0 left-0 [backface-visibility:hidden] flex justify-center items-center h-full w-full transform -rotate-y-360`}>
-  <div className="flex flex-col items-center">
-    {spanish}
-    {urlImage && (
-      <img src={urlImage} alt={spanish} className="mt-4 max-h-52 max-w-full rounded-full" />
-    )}
-  </div>
-</div>
+            <div className={`front p-6 absolute top-0 left-0 [backface-visibility:hidden] flex justify-center items-center h-full w-full transform -rotate-y-360`}>
+              <div className="flex flex-col items-center">
+                <div className="text-2xl">
+                  {spanish}
+                </div>
+                <div>
+                  {urlImage && (
+                    <img src={urlImage} alt={spanish} className="mt-4 max-h-56 max-w-full" />
+                  )}
+                </div>
+                
+            </div>
+            </div>
                   
                 <div className={`back [backface-visibility:hidden] absolute top-0 left-0  flex justify-center items-center h-full w-full transform -rotate-y-180`}>
                     {english}
