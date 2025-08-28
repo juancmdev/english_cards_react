4c42a5c1981504ffa55d7cba5a395e5a03fb0290
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-28 14:24:05 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,71 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        const userData = {
-            username: username,
-            password: password,
-        }
-      
-          e.preventDefault();
-      
-          fetch("http://localhost:5000/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userData),
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-            });
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const userData = {
+      username: username,
+      password: password,
+    };
 
-    }
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }).then((res) => {
+      if (res.ok) {
+        // La respuesta del servidor fue exitosa (código 200-299)
+        // Redirige al usuario a la página de administración
+        navigate("/admin");
+      } else {
+        // La respuesta del servidor indica un error (ej. 401)
+        console.error("Fallo en el inicio de sesión");
+        // Aquí podrías mostrar un mensaje de error al usuario
+      }
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud:", error);
+    });
+    setUsername("");
+    setPassword("");
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-6 bg-gray-100 w-96 m-auto rounded-lg mt-8 mb-8 shadow-lg shadow-gray-400">
-        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required={true} placeholder="Username" className="border border-blue-500 rounded-lg p-2"/>
-        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required={true} minLength={8} placeholder="Password" className="border border-blue-500 rounded-lg p-2"/>
-        <input type="submit" value="Login" className="bg-blue-500 text-white rounded-lg p-2"/>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 p-6 bg-gray-100 w-96 m-auto rounded-lg mt-8 mb-8 shadow-lg shadow-gray-400"
+      >
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required={true}
+          placeholder="Username"
+          className="border border-blue-500 rounded-lg p-2"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required={true}
+          minLength={8}
+          placeholder="Password"
+          className="border border-blue-500 rounded-lg p-2"
+        />
+        <input
+          type="submit"
+          value="Login"
+          className="bg-blue-500 text-white rounded-lg p-2"
+        />
       </form>
     </>
   );
