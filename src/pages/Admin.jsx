d501e197f5364d107b1b1996c25b8f0f01e13d16
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-03 16:32:26 -0500

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -15,8 +15,8 @@ const Admin = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold ">Hello to Admin Page</h1>
-      <button className="">Logout</button>
+      <h1 className="text-3xl font-bold mx-auto text-center mt-4">Hello to Admin Page</h1>
+      <button className="w-30 h-10 text-white rounded-md font-bold p-2 mb-4 mt-4 block mx-auto bg-amber-500 cursor-pointer">Logout</button>
     </>
   );
 };
