daa89c38d7476badf3dd5b95dcecaf5a810ae508
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-10 09:58:09 -0500

    updated

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Admin = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
@@ -22,6 +23,9 @@ const Admin = () => {
   return (
     <>
       <h1 className="text-3xl font-bold mx-auto text-center mt-4">Hello to Admin Page</h1>
+      <h2>Ingresa una nueva tarjeta</h2>
+      <img src="src/assets/iconos/ingresar-datos.png" alt="icono-ingresar-datos" className="w-6 h-6" />
+
       <button className="w-30 h-10 text-white rounded-md font-bold p-2 mb-4 mt-4 block mx-auto bg-amber-500 cursor-pointer"
         onClick={handleLogout}
       >Logout</button>
