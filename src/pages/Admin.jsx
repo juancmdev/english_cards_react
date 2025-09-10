e44bf6e72ca50cc169fd6d5f6992cfb8bdefd973
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-10 10:01:58 -0500

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -23,8 +23,11 @@ const Admin = () => {
   return (
     <>
       <h1 className="text-3xl font-bold mx-auto text-center mt-4">Hello to Admin Page</h1>
-      <h2>Ingresa una nueva tarjeta</h2>
+        
+      <div className="flex">
+      <h2 className="text-xl font-bold mr-3">Ingresa una nueva tarjeta</h2>
       <img src="src/assets/iconos/ingresar-datos.png" alt="icono-ingresar-datos" className="w-6 h-6" />
+      </div>
 
       <button className="w-30 h-10 text-white rounded-md font-bold p-2 mb-4 mt-4 block mx-auto bg-amber-500 cursor-pointer"
         onClick={handleLogout}
