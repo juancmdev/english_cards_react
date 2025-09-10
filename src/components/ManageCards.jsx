02e0d637aac18c53dc01d0ee61145bb3e374e852
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-10 13:21:45 -0500

    Update ManageCards.jsx

--- src/components/ManageCards.jsx
+++ src/components/ManageCards.jsx
@@ -5,15 +5,24 @@ const [cards, setCards] = useState([]);
 
     useEffect(() => {
         try {
-
+            const fetchCards = async () => {
+                const response = await fetch("http://localhost:5000/cards");
+                if(!response.ok) {
+                    throw new Error('No se pudo obtener la lista de tarjetas')
+                }
+                const data = await response.json();
+                setCards(data);
+            }
         } catch (error) {
-            console.log(error);
+            console.error('Error al cargar las tarjetas', error);
         }
+        fetchCards();
     }, [])
 
   return (
     <div>
-      
+      <h1>Administrar Tarjetas</h1>
+      {/* Aquí vamos a mostrar la lista de tarjetas */}
     </div>
   )
 }
