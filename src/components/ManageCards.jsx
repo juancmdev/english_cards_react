512de2e4c7d8c4111e2fdaaf80b35ceaa1fd3ebe
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-10 17:35:00 -0500

    Search bar updated

--- src/components/ManageCards.jsx
+++ src/components/ManageCards.jsx
@@ -1,46 +1,54 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const ManageCards = () => {
-const [cards, setCards] = useState([]);
-const [search, setSearch] = useState('');
+  const [cards, setCards] = useState([]);
+  const [search, setSearch] = useState("");
 
-
-    useEffect(() => {
-        const fetchCards = async () => {
-        try {
-                const response = await fetch("http://localhost:5000/cards?limit=20");
-                if(!response.ok) {
-                    throw new Error('No se pudo obtener la lista de tarjetas')
-                }
-                const data = await response.json();
-                setCards(data);
-            } catch (error) {
-            console.error('Error al cargar las tarjetas', error);
-        };
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/cards?limit=20");
+        if (!response.ok) {
+          throw new Error("No se pudo obtener la lista de tarjetas");
+        }
+        const data = await response.json();
+        setCards(data);
+      } catch (error) {
+        console.error("Error al cargar las tarjetas", error);
+      }
     };
-        fetchCards();
-    }, [])
+    fetchCards();
+  }, []);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mt-6 mb-4">Administrar Tarjetas</h1>
-        <input 
-        className="w-64 h-9 border-1 border-solidborder-black rounded p-2 mb-4 block mx-auto"
-        type="text" 
-        name="search" 
-        placeholder="Buscar" 
-        value={search} 
-        onChange={(e) => setSearch(e.target.value)} 
+      <h1 className="text-3xl font-bold text-center mt-6 mb-4">
+        Administrar Tarjetas
+      </h1>
+      <div className="searchBar flex items-center justify-center border-1 border-solid border-black rounded w-72 h-9 mx-auto mb-4">
+        <input
+          className="w-64 h-9 p-2"
+          type="text"
+          name="search"
+          placeholder="Buscar"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
-      {/* Aquí vamos a mostrar la lista de tarjetas */}
-        {cards.map((card) => (
-            <div key={card._id}>
-                <h3 className="text-xl font-bold mb-2 text-center">{card.spanish}-{card.english}</h3>
-            </div>
-        )
-        )}
-    </div>
-  )
-}
+        <div className="search-icon">
+            <img src="src/assets/iconos/search.png" alt="search-icon" className="w-10 h-10 p-1 cursor-pointer"/>
+        </div>
+      </div>
 
-export default ManageCards
+      {/* Aquí vamos a mostrar la lista de tarjetas */}
+      {cards.map((card) => (
+        <div key={card._id}>
+          <h3 className="text-xl font-bold mb-2 text-center">
+            {card.spanish}-{card.english}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ManageCards;
