5b6a60585853e4ad5cb6d60567d28ef28be135f5
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-09-10 13:14:06 -0500

    Create ManageCards.jsx

---
+++ src/components/ManageCards.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+
+const ManageCards = () => {
+const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        try {
+
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ManageCards
