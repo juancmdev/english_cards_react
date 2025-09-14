05c77a05aba30ccf12abaca5fa6a602b39345f11
juancmdev <juancmorenoj@gmail.com>
2025-09-13 19:26:09 -0500

    Update VerbToHave.jsx

--- src/pages/VerbToHave.jsx
+++ src/pages/VerbToHave.jsx
@@ -42,7 +42,7 @@ const VerbToHave = () => {
 
   return (
     <>
-      <div className="mx-auto bg-blue-300 grid text-center sm:grid-cols-2 whitespace-pre-line md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="mx-auto grid text-center sm:grid-cols-2 whitespace-pre-line md:grid-cols-3 lg:grid-cols-4 gap-4">
         {currentCards.map((card, index) => (
           <Flashcard key={index} cardData={card} />
         ))}
@@ -55,10 +55,13 @@ const VerbToHave = () => {
           Mezclar
         </button>
       </div>
-        
-          
-        <Link to={"/IngresarTarjeta"} className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded cursor-pointer mb-6 mx-auto block w-36">Nueva tarjeta</Link> 
-       
+
+      <Link
+        to={"/IngresarTarjeta"}
+        className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded cursor-pointer mb-6 mx-auto block w-36"
+      >
+        Nueva tarjeta
+      </Link>
     </>
   );
 };
